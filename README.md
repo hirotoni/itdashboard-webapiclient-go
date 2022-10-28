2cadeaf95beb80f7cb7ddeb7047ff2df8e528bae
puddingg <yuki.pdg@gmail.com>
2022-10-28 11:22:42 +0900

    preparation for publishing

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # itdashboard-webapiclient-go
  
-apiclient for the site below written in golang
+apiclient for the site below written in golang.
+Not yet fully implemented. Breaking changes will occur.
 
 https://cio.go.jp/itdashboard/webapi
--- main.go
+++ main.go
@@ -1,36 +1,3 @@
 package main
 
-import (
-	"fmt"
-
-	ac "example.com/my-go-practice/apiclient"
-)
-
-func main() {
-	client, err := ac.NewApiClient()
-	if err != nil {
-		panic(err)
-	}
-
-	options := ac.RequestOptions{
-		FieldsToGet: []ac.BasicInformationField{
-			ac.FieldOrganization,
-			ac.FieldSystemClass,
-			ac.FieldSystemName,
-		},
-		FilterByFields: map[ac.BasicInformationField]string{
-			ac.FieldOrganization: "内閣府",
-		},
-	}
-
-	result, err := client.FetchBasicInformation(options)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println(result.Info)
-
-	for i, v := range result.RawData {
-		fmt.Println(i, v)
-	}
-}
+func init() {}
